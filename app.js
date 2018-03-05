2d748a5f4ffb6997de5bcc0546fc539dd904eed4
Paul Jung <pauljung@Pauls-MacBook-Pro.local>
2018-03-04 19:43:14 -0500

    fourth commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
-app.get('/', (req,res) =>res.send(process.env));
+app.get('/', (req,res) =>res.send(process.env.APPSETTING_MyAppSettings + ' AND '+ process.env.CUSTOMCONNSTR_MyConnectionString));
 app.listen (port, () => console.log('Server is running on port'+port));
